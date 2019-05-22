8dd33e73049059269b7a9db5e2f6fa913019e04f
msanad <msanad@ezzsteel.com.eg>
2019-05-22 10:54:46 +0200

    Commiting Others

---
+++ twitts/src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,11 @@
+<h2>A JSP page </h2>
+<%@ page language="java"
+    contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<%@taglib uri="http://www.springframework.org/tags" prefix="spring"%>
+
+<html>
+<body>
+    <p>This page can be updated in real time without restarting</p>
+</body>
+</html>
---
+++ twitts/src/test/java/com/ezdk/TwittsApplicationTests.java
@@ -0,0 +1,17 @@
+package com.ezdk;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TwittsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
