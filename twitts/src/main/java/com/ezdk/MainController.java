541e7485b0569cde4031a2a73ef887d7f9761144
msanad <msanad@ezzsteel.com.eg>
2019-05-22 11:01:33 +0200

    Adding comment
    Signed-off-by: msanad <msanad@ezzsteel.com.eg>

--- twitts/src/main/java/com/ezdk/MainController.java
+++ twitts/src/main/java/com/ezdk/MainController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Controller
 @RequestMapping({"/","/home"})
 public class MainController {
+	//Testing History changes
 	@RequestMapping(method=RequestMethod.GET)
 	public String home() {
 		System.out.println(2);
