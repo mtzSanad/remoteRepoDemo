fef9e7f40395618bfc19f1ae17b9fd1498b964af
Moataz Sanad <mtz.sanad@gmail.com>
2019-05-26 12:49:54 +0200

    MathHelper

--- CalculatorGitDemo/src/MathHelper.java
+++ CalculatorGitDemo/src/MathHelper.java
@@ -1,8 +1,6 @@
 
 public class MathHelper {
-	//Push to Repo
-	//Version at Index
-	//Version at Working Directory
-	//Adding master Math Helper
-	//Adding master 2
+	//dfsfsfdskfds
+	df
+	sdfsdf
 }
