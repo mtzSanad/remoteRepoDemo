6cadb388dbb1fa02b048d7d91a535d3ddfacff05
Moataz Sanad <mtz.sanad@gmail.com>
2019-05-26 12:44:54 +0200

    Delete and add master 2 MathHelper

--- CalculatorGitDemo/src/MathHelper.java
+++ CalculatorGitDemo/src/MathHelper.java
@@ -1,11 +1,8 @@
 
 public class MathHelper {
-	//Adding without conflict
-	public int sum(int a, int b) {
-		return 0;
-	}
 	//Push to Repo
 	//Version at Index
 	//Version at Working Directory
 	//Adding master Math Helper
+	//Adding master 2
 }
