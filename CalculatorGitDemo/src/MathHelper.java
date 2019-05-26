7507364501d267e47840c62a445cbb304f545082
Moataz Sanad <mtz.sanad@gmail.com>
2019-05-26 13:43:06 +0200

    Math helper cont

--- CalculatorGitDemo/src/MathHelper.java
+++ CalculatorGitDemo/src/MathHelper.java
@@ -6,17 +6,17 @@ public class MathHelper {
 	//Push to Repo
 	//Fix this
 	//FFFFFFFFFFFFFFFFFF
-<<<<<<< HEAD
+
 		//Fix this
 		//FFFFFFFFFFFFFFFFFF
 		//Version at Index
 		//Version at Working Directory
 		//Adding master Math Helper
 		//Adding master 2          
-=======
+
 	//Version at Index
 	//Version at Working Directory
 	//Adding master Math Helper
 	//Adding master 2
->>>>>>> refs/heads/Branch1
+
 }
