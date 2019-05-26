e718b129c5edbd98a8a5084a03a57bfa6711beb5
Moataz Sanad <mtz.sanad@gmail.com>
2019-05-26 12:51:38 +0200

    merge commit

--- CalculatorGitDemo/src/MathHelper.java
+++ CalculatorGitDemo/src/MathHelper.java
@@ -3,4 +3,13 @@ public class MathHelper {
 	//dfsfsfdskfds
 	df
 	sdfsdf
+	//Push to Repo
+	//Fix this
+	//FFFFFFFFFFFFFFFFFF
+		//Fix this
+		//FFFFFFFFFFFFFFFFFF
+		//Version at Index
+		//Version at Working Directory
+		//Adding master Math Helper
+		//Adding master 2          
 }
